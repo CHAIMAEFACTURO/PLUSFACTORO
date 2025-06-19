95fea97948141a3c2df30e422df6d4a475cc392c
CHAIMAEFACTURO <aitaddichayma@gmail.com>
2025-06-19 21:41:20 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>+Facturo</title>
+</head>
+<body>
+  <h1>Bienvenue sur +Facturo</h1>
+  <a href="facture.php">Ajouter une facture</a> | 
+  <a href="list.php">Voir les factures</a>
+</body>
+</html>
